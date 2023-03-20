81f6f3b8090d83f1cc53a66505fd86904c612abb
devZaidGit <developer.mzaid@gmail.com>
2023-03-21 00:18:09 +0500

    comment the code

--- app.js
+++ app.js
@@ -16,20 +16,20 @@ app.get('/', (req, res) => res.send('Welcome to the Nodejs Project'))
 
 app.post('/auth', (req, res) => {
     try {
-        const accessKey = 'HZNwQfMQueiTQob1XRXJ';
-       let query = '';
-      query += `accessId=ruO7vifU7wzB2fVw9uSA`;
-      query += `&merchantId=1029`;
-      query += `&description=transaction description`;
-      query += `&currency=USD`;
-      query += `&amount=9.98`;
-      query += `&displayAmount=0.00`;
-      query += `&minimumBalance=0.00`;
-      query += `&merchantReference=1679160133`;
-      query += `&paymentType=Deferred`;
-      query += `&timeZone=`;
+      //   const accessKey = 'HZNwQfMQueiTQob1XRXJ';
+      //  let query = '';
+      // query += `accessId=ruO7vifU7wzB2fVw9uSA`;
+      // query += `&merchantId=1029`;
+      // query += `&description=transaction description`;
+      // query += `&currency=USD`;
+      // query += `&amount=9.98`;
+      // query += `&displayAmount=0.00`;
+      // query += `&minimumBalance=0.00`;
+      // query += `&merchantReference=1679160133`;
+      // query += `&paymentType=Deferred`;
+      // query += `&timeZone=`;
 
-        // const { accessKey, query } = req.body;
+        const { accessKey, query } = req.body;
         const requestSignature = Crypto.createHmac('sha1', accessKey).update(query).digest('base64');
 
         res.setHeader('Content-Type', 'application/json');
